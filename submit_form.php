3caa1e5d1c956f296d2adbd0283c46c68b72caa8
Healthtraveler <154063996+Healthtraveler@users.noreply.github.com>
2024-02-04 11:23:24 -0800

    Update submit_form.php

--- submit_form.php
+++ submit_form.php
@@ -9,7 +9,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $appointmentTime = $_POST["appointmentTime"];
 
     // Email configuration (update with your own details)
-    $to = "your-email@example.com"; // Specify your email address here
+    $to = "nelsonquinteros@ymail.com"; // Specify your email address here
     $subject = "New Appointment Booking";
     $headers = "From: $email";
 
