6ef683c72610626f3d2249abcd4864c58eab14d7
Healthtraveler <154063996+Healthtraveler@users.noreply.github.com>
2024-02-09 00:39:06 -0800

    Delete your-form-validation-script.js

--- your-form-validation-script.js
+++
@@ -1,9 +0,0 @@
-function submitForm() {
-    // Validate the form data if needed
-
-    // Hide the form
-    document.getElementById('formContainer').style.display = 'none';
-
-    // Show the thank you message
-    document.getElementById('thankYouMessage').style.display = 'block';
-}
